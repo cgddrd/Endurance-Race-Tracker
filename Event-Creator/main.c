98abfce318b2d82f5e82560033a30047ec72a614
Connor Goddard <clg11@aber.ac.uk>
2013-02-26 20:01:00 +0000

    Added other project folders

---
+++ Event-Creator/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: connor
+ *
+ * Created on February 26, 2013, 8:00 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ Event-Manager/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: connor
+ *
+ * Created on February 26, 2013, 7:58 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "hello";
+
+    return 0;
+}
+
